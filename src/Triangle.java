be7b0919c4ffd3f79f1eaee98f58b76f722002fc
Tahamid <s2110376138@ru.ac.bd>
2022-09-25 12:28:13 +0530

    Trinagle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,179 @@
 public class Triangle {
+    /**
+     * Declare the private variables
+     */
+
+
+     private double x1, y1, x2, y2, x3, y3;
+
+
+     /**
+     Create a constructor to get the coordinates of the triangle
+     */
+    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+
+    {
+
+        this.x1 = x1;
+
+        this.y1 = y1;
+
+        this.x2 = x2;
+
+        this.y2 = y2;
+
+        this.x3 = x3;
+
+        this.y3 = y3;
+
+    }
+
+
+    /**
+     *  Function definition to get the length of side 1
+     */
+    public double getSide1Length()
+
+    {
+
+        /**
+         * Return the length of side 1
+         */
+
+        return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+
+    }
+
+    /**
+     *  Function definition to get the length of side 2
+     */
+
+    public double getSide2Length()
+
+    {
+        /**
+         * Return the length of side 2
+         */
+
+        return Math.sqrt(Math.pow((x1 - x3), 2) + Math.pow((y1 - y3), 2));
+
+    }
+
+    /**
+     * Function definition to get the length of side 3
+     */
+
+    public double getSide3Length()
+
+    {
+
+        /**
+         *  Return the length of side 3
+         */
+
+        return Math.sqrt(Math.pow((x2 - x3), 2) + Math.pow((y2 - y3), 2));
+
+    }
+
+    /**
+     * Function definition to get the angle of corner 1
+     */
+
+    public double getAngle1()
+
+    {
+
+        double a = getSide3Length();
+
+        double b = getSide1Length();
+
+        double c = getSide2Length();
+
+        /**
+         * Compute and return the angle of corner 1
+         */
+
+        return Math.acos(((b * b) + (c * c) - (a * a)) / (2 * b * c));
+
+    }
+
+    /**
+     *  Function definition to get the angle of corner 2
+     */
+
+    public double getAngle2()
+
+    {
+
+        double a = getSide3Length();
+
+        double b = getSide1Length();
+
+        double c = getSide2Length();
+
+        // Compute and return the angle of corner 2
+
+        return Math.acos(((b * b) + (a * a) - (c * c)) / (2 * b * a));
+
+    }
+
+    /**
+     *  Function definition to get the angle of corner 3
+     */
+    public double getAngle3()
+
+    {
+
+        double a = getSide3Length();
+
+        double b = getSide1Length();
+
+        double c = getSide2Length();
+
+        // Compute and return the angle of corner 3
+
+        return Math.acos(((a * a) + (c * c) - (b * b)) / (2 * a * c));
+
+    }
+
+
+    /**
+     *  Function definition to get the  perimeter of a triangle
+     */
+
+    public double getPerimeter()
+
+    {
+
+        /**
+         * Compute and return the perimeter of a triangle
+         */
+
+        return getSide1Length() + getSide2Length() + getSide3Length();
+
+    }
+
+    /**
+     *  Function definition to get the  area of a triangle
+     */
+
+    public double getArea()
+
+    {
+
+        double s = getPerimeter() / 2.0;
+
+        double a = getSide1Length();
+
+        double b = getSide2Length();
+
+        double c = getSide3Length();
+
+        /**
+         * Compute and return the area of a triangle
+         */
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+    }
+
 }
--- src/TrianglePropertyPrinter.java
+++ src/TrianglePropertyPrinter.java
@@ -1,2 +1,103 @@
-package PACKAGE_NAME;public class TrianglePropertyPrinter {
+/** Importing input package
+ *
+ * */
+
+import java.util.Scanner;
+
+/**
+ * Class definition
+ */
+public class TrianglePropertyPrinter {
+
+    /** Main method definition
+     *
+     */
+
+    public static void main(String[] args)
+
+    {
+
+/**
+ *Creating scanner class object to read user input
+ *
+ */
+
+        Scanner scan = new Scanner(System.in);
+
+        /** Asking the user to enter x and y coordinates
+         */
+
+        System.out.println("Enter the x,y coordinates of three points in this order x1,y1,x2,y2,x3,y3: ");
+
+
+/**
+ * Declare the required variables to store x and y coordinate values
+ */
+        double x1 = scan.nextDouble();
+
+        double y1 = scan.nextDouble();
+
+        double x2 = scan.nextDouble();
+
+        double y2 = scan.nextDouble();
+
+        double x3 = scan.nextDouble();
+
+        double y3 = scan.nextDouble();
+
+        /**
+         * Create an object for Triangle class to determine the length of all three sides, the angle of all three corners, the perimeter and area of a triangle
+         *
+         **/
+
+        Triangle triangle = new Triangle(x1, y1, x2, y2, x3, y3);
+
+
+        /**
+         * Print the length of side 1
+         */
+        System.out.println("Side 1 length:  " + triangle.getSide1Length());
+
+        /**
+         * Print the length of side
+         */
+
+        System.out.println("Side 2 length:  " + triangle.getSide2Length());
+
+        /**
+         * Print the length of side 3
+         */
+
+        System.out.println("Side 3 length:  " + triangle.getSide3Length());
+
+        /**
+         * Print the angle of corner 1
+         */
+        System.out.println("Angle 1:  " + triangle.getAngle1());
+
+        /**
+         * Print the angle of corner 2
+         */
+        System.out.println("Angle 2:  " + triangle.getAngle2());
+
+        /**
+         * Print the angle of corner 3
+         */
+
+        System.out.println("Angle 3:  " + triangle.getAngle3());
+
+
+        /***
+         Print the perimeter of triangle
+         */
+        System.out.println("The perimeter of the triangle is " + triangle.getPerimeter());
+
+        /***
+         Print the area of triangle
+         */
+
+        System.out.println("The area of the triangle is " + triangle.getArea());
+
+    }
+
 }
